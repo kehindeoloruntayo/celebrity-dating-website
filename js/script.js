7bd9bf42f06315f3378d207e983d80f56c2fe45e
Kehinde Oloruntayo <kehindeoloruntayo1@gmail.com>
2023-08-15 15:25:33 +0100

    make changes to jQuery functionalities

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $("#genderform").submit(function(event) {
+    $("#genderform").submit(function (event) {
         event.preventDefault();
-        const gender  = $("select#gender").val();
+        const gender = $("select#gender").val();
         const nameInput = $("input#uname").val();
 
         if (gender === 'male') {
@@ -26,37 +26,37 @@ $(document).ready(function () {
         if (personality === "introvert" && colourInput === "blue" && petInput === "cat" && shapeInput === "curvy" && heightInput === "tall" && skinInput === "black") {
             $("#male-form").hide();
             $(".match1").show(1000);
-        } else if (personality === "extrovert" && colourInput === "black" && petInput === "dog" && shapeInput === "slim" && (heightInput === "average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "extrovert" && colourInput === "black" && petInput === "dog" && shapeInput === "slim" && heightInput === "average" && skinInput === "black") {
             $("#male-form").hide();
             $(".match2").show(1000);
-        } else if (personality === "introvert" && colourInput === "white" && petInput === "dog" && shapeInput === "slim" && (heightInput === "average" || heightInput === "short") && skinInput === "white") {
+        } else if (personality === "introvert" && colourInput === "white" && petInput === "dog" && shapeInput === "slim" && heightInput === "short" && skinInput === "white") {
             $("#male-form").hide();
             $(".match11").show(1000);
-        } else if (personality === "extrovert" && colourInput === "blue" && petInput === "dog" && shapeInput === "slim" && (heightInput === " average" || heightInput === "tall") && skinInput === "white") {
+        } else if (personality === "extrovert" && colourInput === "blue" && petInput === "dog" && shapeInput === "slim" && heightInput === "tall" && skinInput === "white") {
             $("#male-form").hide();
             $(".match10").show(1000);
-        } else if (personality === "introvert" &&   colourInput === "white" && petInput === "cat" && shapeInput === "curvy" && (heightInput === " average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "introvert" && colourInput === "white" && petInput === "cat" && shapeInput === "curvy" && heightInput === " average" && skinInput === "black") {
             $("#male-form").hide();
             $(".match8").show(1000);
-        } else if (personality === "introvert" && colourInput === "black" && petInput === "cat" && shapeInput === "curvy"  && (heightInput === " average" || heightInput === "short") && skinInput === " white") {
+        } else if (personality === "introvert" && colourInput === "black" && petInput === "cat" && shapeInput === "curvy" && heightInput === "short" && skinInput === " white") {
             $("#male-form").hide();
             $(".match12").show(1000);
-        } else if ((personality === "extrovert" || personality === "introvert") && colourInput === "blue" && petInput === "cat" && shapeInput === "curvy" && (heightInput === " average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "extrovert" && colourInput === "blue" && petInput === "cat" && shapeInput === "curvy" && heightInput === "tall" && skinInput === "black") {
             $("#male-form").hide();
             $(".match5").show(1000);
-        } else if (personality === "introvert" && colourInput === "black" && petInput === "cat" && shapeInput === "curvy" && heightInput === "tall" && skinInput === "black") {
+        } else if (personality === "introvert" && colourInput === "black" && petInput === "cat" && shapeInput === "curvy" && heightInput === "average" && skinInput === "black") {
             $("#male-form").hide();
             $(".match7").show(1000);
-        } else if (personality === "extrovert" && colourInput === "white" && petInput === "dog" && shapeInput === "slim" && (heightInput === " average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "extrovert" && colourInput === "white" && petInput === "dog" && shapeInput === "slim" && heightInput === "short" && skinInput === "black") {
             $("#male-form").hide();
             $(".match3").show(1000);
-        } else if (personality === "extrovert" && (colourInput === "black" || colourInput === "blue" || colourInput === "white") && petInput === "cat" && shapeInput === "slim" && (heightInput === " average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "extrovert" && (colourInput === "black" || colourInput === "blue" || colourInput === "white") && petInput === "cat" && shapeInput === "slim" && heightInput === "short" && skinInput === "black") {
             $("#male-form").hide();
             $(".match6").show(1000);
-        } else if (personality === "introvert" && colourInput === "black" && petInput === "dog" && shapeInput === "slim" && (heightInput === " average" || heightInput === "short") && skinInput === "black") {
+        } else if (personality === "introvert" && colourInput === "black" && petInput === "dog" && shapeInput === "slim" && heightInput === "tall" && skinInput === "black") {
             $("#male-form").hide();
             $(".match4").show(1000);
-        } else if (personality === "extrovert" && colourInput === "blue" && petInput === "dog" && shapeInput === "curvy" && heightInput === "average" && skinInput === "black") {
+        } else {
             $("#male-form").hide();
             $(".match9").show(1000);
         }
@@ -74,39 +74,42 @@ $(document).ready(function () {
         if (attribute === "introvert" && faveColour === "pink" && favePet === "cat" && buildInput === "muscular" && inputtedHeight === "tall" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match1").show(1000);
-        } else if (attribute === "extrovert" && faveColour === "yellow" && favePet === "dog" && buildInput === "slim" && (inputtedHeight === "average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "extrovert" && faveColour === "yellow" && favePet === "dog" && buildInput === "slim" && inputtedHeight === "average" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match2").show(1000);
-        } else if (attribute === "introvert" && faveColour === "orange" && favePet === "dog" && buildInput === "slim" && (inputtedHeight === "average" || inputtedHeight === "short") && complexionInput === "white") {
+        } else if (attribute === "introvert" && faveColour === "orange" && favePet === "dog" && buildInput === "slim" && inputtedHeight === "average" && complexionInput === "white") {
             $("#female-form").hide();
             $(".match11").show(1000);
-        } else if (attribute === "extrovert" && faveColour === "pink" && favePet === "dog" && buildInput === "slim" && (inputtedHeight === " average" || inputtedHeight === "tall") && complexionInput === "white") {
+        } else if (attribute === "extrovert" && faveColour === "pink" && favePet === "dog" && buildInput === "slim" && inputtedHeight === "tall" && complexionInput === "white") {
             $("#female-form").hide();
             $(".match10").show(1000);
-        } else if (attribute === "introvert" &&   faveColour === "orange" && favePet === "cat" && buildInput === "muscular" && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "introvert" && faveColour === "orange" && favePet === "cat" && buildInput === "muscular" && inputtedHeight === " average" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match8").show(1000);
-        } else if (attribute === "introvert" && faveColour === "yellow" && favePet === "cat" && buildInput === "muscular"  && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === " white") {
+        } else if (attribute === "introvert" && faveColour === "yellow" && favePet === "cat" && buildInput === "muscular" && inputtedHeight === " average" && complexionInput === " white") {
             $("#female-form").hide();
             $(".match12").show(1000);
-        } else if ((attribute === "extrovert" || attribute === "introvert") && faveColour === "pink" && favePet === "cat" && buildInput === "muscular" && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "extrovert" && faveColour === "pink" && favePet === "cat" && buildInput === "muscular" && inputtedHeight === "short" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match5").show(1000);
         } else if (attribute === "introvert" && faveColour === "yellow" && favePet === "cat" && buildInput === "muscular" && inputtedHeight === "tall" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match7").show(1000);
-        } else if (attribute === "extrovert" && faveColour === "orange" && favePet === "dog" && buildInput === "slim" && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "extrovert" && faveColour === "orange" && favePet === "dog" && buildInput === "slim" && inputtedHeight === " average" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match3").show(1000);
-        } else if (attribute === "extrovert" && (faveColour === "yellow" || faveColour === "pink" || faveColour === "white") && favePet === "cat" && buildInput === "slim" && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "extrovert" && faveColour === "orange" && favePet === "cat" && buildInput === "slim" && inputtedHeight === "average" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match6").show(1000);
-        } else if (attribute === "introvert" && faveColour === "yellow" && favePet === "dog" && buildInput === "slim" && (inputtedHeight === " average" || inputtedHeight === "short") && complexionInput === "black") {
+        } else if (attribute === "introvert" && faveColour === "yellow" && favePet === "dog" && buildInput === "slim" && inputtedHeight === "tall" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match4").show(1000);
-        } else if (attribute === "extrovert" && faveColour === "pink" && favePet === "dog" && buildInput === "muscular" && inputtedHeight === "average" && complexionInput === "black") {
+        } else if (attribute === "extrovert" && faveColour === "pink" && favePet === "dog" && buildInput === "muscular" && inputtedHeight === "short" && complexionInput === "black") {
             $("#female-form").hide();
             $(".match9").show(1000);
+        } else {
+            $("#female-form").hide();
+            $(".match10").show(1000);
         }
     });
 });
